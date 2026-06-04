c31d8f28c049c1606034b7eac9a5a1d1c730edd2
Thomas <thomas@ledube.fr>
2026-06-04 17:41:52 +0200

    IHM : Ajout de la fenêtre de jeu (FenetreJeu) et de la structure de la grille en UniformGrid

---
+++ SAE-IHM/FenetreJeu.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SAE_IHM
+{
+    public partial class FenetreJeu : Window
+    {
+        public FenetreJeu()
+        {
+            InitializeComponent();
+        }
+
+        // Cet événement se déclenche quand un joueur clique sur une case de la grille
+        private void Case_Click(object sender, RoutedEventArgs e)
+        {
+            // Grâce au sender, on pourra identifier dans quelle colonne le joueur a cliqué !
+            // (La logique sera ajoutée ici)
+        }
+    }
+}
