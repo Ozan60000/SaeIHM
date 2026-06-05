38b38cf99389d5f8dfb9db27f7ca4690ed2684bb
Ozan Aytac <iphoneozan60@gmail.com>
2026-06-05 14:12:34 +0200

    Création de la fenêtre Regles

---
+++ SAE-IHM/Regles.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SAE_IHM
+{
+    public partial class Regles : Window
+    {
+        // Propriétés lues par la MainWindow après validation
+        public int NbLignes { get; private set; }
+        public int NbColonnes { get; private set; }
+        public int NbAAligner { get; private set; }
+
+        public Regles()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnValider_Click(object sender, RoutedEventArgs e)
+        {
+            // Récupération des valeurs sélectionnées
+            NbLignes = Convert.ToInt32(((ComboBoxItem)CboLignes.SelectedItem).Content);
+            NbColonnes = Convert.ToInt32(((ComboBoxItem)CboColonnes.SelectedItem).Content);
+            NbAAligner = Convert.ToInt32(((ComboBoxItem)CboAlignement.SelectedItem).Content);
+
+            this.DialogResult = true;
+        }
+
+        private void BtnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
